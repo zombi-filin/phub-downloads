d25c4b338262964d56ee7bfc293a6aa8b6e34bc2
zombi-filin <zombi.filin.256@gmail.com>
2025-11-17 23:23:31 +0300

    Переделка на viewkeys

--- main.py
+++ main.py
@@ -6,58 +6,74 @@ import json
 import time
 import re
 
+# Имя каталога куда будут скачиваться файлы
 DOWNLOAD_FOLDER_NAME = 'save'
-URL_LIST_FILE_NAME = 'url.list'
 
+# Имя файла со списком viewkeys
+URL_LIST_FILE_NAME = 'viewkeys'
 
-download_urls = []
+# Список viewkeys
+viewkeys_list = []
 
+# Проверка существования viewkeys
 if not os.path.exists(URL_LIST_FILE_NAME):
+    # Создание файла списка viewkeys
     open(URL_LIST_FILE_NAME,"w")
     print(f'Write url in {URL_LIST_FILE_NAME}')
 else:
+    # Чтение viewkeys
     with open(URL_LIST_FILE_NAME) as f:
         for line in f:
-            download_urls.append(line.replace('\n',''))
+            viewkeys_list.append(line.replace('\n',''))
 
+# Создание каталога загрузк при отсутсвии
 if not os.path.exists(DOWNLOAD_FOLDER_NAME):
     os.mkdir(DOWNLOAD_FOLDER_NAME)
-    
+
+# Смена текущего каталога
 os.chdir(DOWNLOAD_FOLDER_NAME)
 
+# Счетчики
 timeout_count = 0
 remove_count = 0
 total_count = 0
 
-for url in download_urls:
+# Проход по списку viewkeys
+for viewkeys in viewkeys_list:
     total_count += 1
 
+    # Авторизация
     client = phub.Client(login=config.LOGIN,password=config.PASSWORD, change_title_language = False)
     
-    video = client.get(url)
+    # Запрос видео
+    video = client.get(f'https://www.pornhub.org/view_video.php?viewkey={viewkeys}')
     
+    # Длительность видео
     video_duration = video.duration.seconds
     
-    
+    # Заголовок видео
     video_title = re.sub(r'[^a-zA-Z0-9\s]', '', video.title)
 
+    # Имя выходного фала
     file_name = f'{video.key}-{video_title}.mp4'.replace('?','').replace(' ','_')
-    
     print(f'{file_name}')
     
-
     if os.path.exists(file_name):
+        # Видео файл существует
         print(f'{file_name} exist')
     else:
+        # Ссылки на файлы потоков
         m3u8_url_720 = None
         m3u8_url_480 = None
 
+        # Проход по спискам потоков
         for (width, height), uri in video.get_m3u8_urls.items():
             if width == 480 or height == 480:
                 m3u8_url_480 = uri
             if width == 0 or width== 720 or height == 720:
                 m3u8_url_720 = uri
 
+        # Выбор потока
         if m3u8_url_720 is not None:
             m3u8_url = m3u8_url_720
         elif m3u8_url_480 is not None:
@@ -67,30 +83,35 @@ for url in download_urls:
 
         print(m3u8_url)
 
+        # Команда загрузки потока
         command = ['ffmpeg', '-i', m3u8_url, '-c', 'copy', '-bsf:a', 'aac_adtstoasc', file_name]
         
+        # Запуск команды
         with subprocess.Popen(command) as proc:
             try:
+                # Таймаут выполнения команды равно длительности видео
                 proc.wait(timeout = video_duration)
             except subprocess.TimeoutExpired:
+                # Сработал таймаут
                 timeout_count += 1
                 proc.terminate()
                 proc.wait()
-        
-        time.sleep(5)
     
-    #
-    
+    # Запроса статистики скаченного видео
     command = f'ffprobe -print_format json -show_format -show_streams {file_name}'
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
     result_json = json.loads(result.stdout)
-        
+
+    # Проверка дельты между оригиналом и скаченным видео
     if len(result_json)==0 or abs(video_duration - int(float(result_json['format']['duration']))) > 2:
+        # Если разница большая удаляем скаченный файл
         print(f'remove {file_name}')
         if os.path.exists(file_name):
             os.remove(file_name)
             remove_count += 1
     
-    
+# Вывод статистики
 print(f'total:{total_count} timeout:{timeout_count} removed:{remove_count}')
+
+# Конец скрипта
 print('DONE')
